2a1aced6da1f28b668fd118ef16b0ee64a425518
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-07-14 01:20:34 +0600

    added testimonial page

--- lp2/testimonials.php
+++ lp2/testimonials.php
@@ -31,21 +31,9 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-sm-8">
-                                <h1 class="site-header text-center">Sell House Fast - Guaranteed Offer in 24 Hours</h1>
+                                <h1 class="site-header">Testimonials</h1>
 
-                                <div id="site-carousel" class="carousel slide" data-ride="carousel">
-                                    <div class="carousel-inner" role="listbox">
-                                        <div class="item active">
-                                            <img src="images/assets/img/slideV3_1.png" width="633" height="290">
-                                        </div>
-                                        <div class="item">
-                                            <img src="images/assets/img/slideV3_2.png" width="633" height="290">
-                                        </div>
-                                        <div class="item">
-                                            <img src="images/assets/img/slideV3_3.png" width="633" height="290">
-                                        </div>
-                                    </div>
-                                </div>
+                                <img src="images/assets/img/dont-just-take-our-words.png" class="img-responsive" alt="">
                             </div>
                             <div class="col-sm-4">
                                 <div class="offer-form">
@@ -73,115 +61,62 @@
                         </div>
                     </div>
                 </div>
-                <div class="box">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="card">
-                                    <h3 class="card-header">Selling Has Never Been Easier</h3>
-                                    <ul class="tick-list">
-                                        <li><span class="sprite sprite-green-check"></span> Get up to 100% value</li>
-                                        <li><span class="sprite sprite-green-check"></span> Free consultation, free valuation</li>
-                                        <li><span class="sprite sprite-green-check"></span> Any house, any value, in any condition</li>
-                                        <li><span class="sprite sprite-green-check"></span> Sell in days, instead of months</li>
-                                        <li><span class="sprite sprite-green-check"></span> Repossession Stopped</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="card card-testimonials">
-                                    <div class="badge sprite sprite-card_badge"></div>
-                                    <h3 class="card-header">Testimonials <span class="sprite sprite-testi-icon"></span></h3>
-
-                                    <div id="testimonial-carousel" class="carousel slide" data-ride="carousel">
-                                        <div class="carousel-inner" role="listbox">
-                                            <div class="item active">
-                                                <p><span class="sprite sprite-left-quote"></span> I had a really good experience with Springbok, from start to finish they kept me informed every step of the way, their advice was invaluable.</p>
-                                                <footer>Cathy Manning</footer>
-                                            </div>
-                                            <div class="item">
-                                                <p><span class="sprite sprite-left-quote"></span> Springbok dealt with myself and my family as we had to sell an inherited property. They were very sensitive towards us during this time and the support we received during the selling process was good and efficient!</p>
-                                                <footer>A James</footer>
-                                            </div>
-                                            <div class="item">
-                                                <p><span class="sprite sprite-left-quote"></span> I hadn't heard of this firm before, but I really must say that they handled the sale of my old house in a very professional, efficient and pleasant manner at all times. Normally, I don't leave remarks regarding business but felt I just had to in this particular instance as they are that good.</p>
-                                                <footer>David Brock</footer>
-                                            </div>
-                                            <div class="item">
-                                                <p><span class="sprite sprite-left-quote"></span> After trying to sell my property with other firms I came across Springbok and I'm so glad I did. They were very helpful and always kept me up to date with everything that went on.</p>
-                                                <footer>Dean Hankin</footer>
-                                            </div>
-                                            <div class="item">
-                                                <p><span class="sprite sprite-left-quote"></span> Springbok have been great with my sale. Offering good advice and excellent customer service throughout the whole process. </p>
-                                                <footer>L Hankin</footer>
-                                            </div>
-                                            <div class="item">
-                                                <p><span class="sprite sprite-left-quote"></span> I like their fast & friendly service, and will not hesitate in recommending Springbok to anyone</p>
-                                                <footer>Steve Boylan</footer>
-                                            </div>
-                                            <div class="item">
-                                                <p><span class="sprite sprite-left-quote"></span> I found Springbok very helpful, very co-operative with every question I asked, was pleased with their overall sale of the property, I would recommend them to anyone who is thinking of selling their property, very pleased.</p>
-                                                <footer>Mr. G Grimes</footer>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="steps hidden-xs">
-                            <img src="images/assets/img/steps.png" width="854" height="242">
-                        </div>
-                    </div>
-                </div>
 
                 <div class="box">
                     <div class="container-fluid">
-                        <h3>We Buy Any House - Sell Your House with Yellow Springbok</h3>
+                        <h3>Meet a few of our happy customers from all around the UK.</h3>
 
-                        <div class="row">
-                            <div class="col-sm-8">
-                                <p>Do you want to sell your house or a second property, but don't want to wait months? Yellow Springbok provides fast property buying services all over the UK -- simply, confidentially and on your timescale.</p>
+                        <p>&nbsp;</p>
 
-                                <p>Maybe you've inherited a property. Perhaps you're in danger of having your own home repossessed. Maybe you're moving, and just need cash quickly. Whatever your situation is, Yellow Springbok can help.</p>
+                        <div class="testimonial">
+                            <div class="testimonial-title">
+                                <img src="images/assets/img/star.png">
+                                <img src="images/assets/img/star.png">
+                                <span>John lennon</span>
                             </div>
-                            <div class="col-sm-4 text-center">
-                                <img src="images/assets/img/fast_sale_max.png" width="291" height="75">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="box no-bottom">
-                    <div class="container-fluid">
-                        <h3>Sell House Fast - Get a no-obligation cash offer by completing the form, or call us now</h3>
-
-                        <p>We understand that selling a house can be a difficult decision, especially if you need a quick sale. That's why the Yellow Springbok is adamant that your peace of mind is just as important as our continued operation.</p>
-
-                        <p>We always try to find the best solution and the best selling price for you - guaranteed. We offer all these advantages:</p>
-
-                        <div class="row">
-                            <div class="col-sm-8">
-                                <ul>
-                                    <li>Achieve 95-100% of market value for your house</li>
-                                    <li>Sell in weeks, not months</li>
-                                    <li>Sell almost any property in any condition, anywhere in the UK</li>
-                                    <li>Free no-obligation valuation & consultation</li>
-                                    <li>Tailor-made solutions, suitable to your specific situation</li>
-                                    <li>No need to decorate or refurbish</li>
-                                    <li>100% no-obligation offer</li>
-                                    <li>Quick decision</li>
-                                    <li>Simple and hassle-free process</li>
-                                    <li>Totally confidential, whether you contact us by phone or online</li>
-                                </ul>
-                            </div>
-                            <div class="col-sm-4">
-                                <img src="images/assets/img/100.png" width="170" height="170">
+                            <div class="testimonial-content">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, mollitia, omnis. Atque beatae deserunt doloribus est neque qui quidem tempore?
                             </div>
                         </div>
+
+                        <div class="testimonial">
+                            <div class="testimonial-title">
+                                <img src="images/assets/img/star.png">
+                                <img src="images/assets/img/star.png">
+                                <img src="images/assets/img/star.png">
+                                <span>John lennon</span>
+                            </div>
+                            <div class="testimonial-content">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, mollitia, omnis. Atque beatae deserunt doloribus est neque qui quidem tempore?
+                            </div>
+                        </div>
+
+                        <div class="testimonial">
+                            <div class="testimonial-title">
+                                <img src="images/assets/img/star.png">
+                                <img src="images/assets/img/star.png">
+                                <img src="images/assets/img/star.png">
+                                <span>John lennon</span>
+                            </div>
+                            <div class="testimonial-content">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, mollitia, omnis. Atque beatae deserunt doloribus est neque qui quidem tempore?
+                            </div>
+                        </div>
+
+                        <div class="testimonial">
+                            <div class="testimonial-title">
+                                <img src="images/assets/img/star.png">
+                                <img src="images/assets/img/star.png">
+                                <img src="images/assets/img/star.png">
+                                <span>John lennon</span>
+                            </div>
+                            <div class="testimonial-content">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, mollitia, omnis. Atque beatae deserunt doloribus est neque qui quidem tempore?
+                            </div>
+                        </div>
+
                     </div>
                 </div>
-
 
             </section>
         </div>
