d8e0cf25de87eebd7e6c9e866586e473d47afb5c
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-07-21 20:28:18 +0600

    Added two inner pages

---
+++ sprevamp/how-much-is-my-house-worth.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="house-worth-banner sp-secondary-text">
+            <div class="container">
+                <form class="estimate-form center-align classic-form white">
+                    <h3>How much is my house worth?</h3>
+                    <p class="center-align big">
+                        Find out for free<br>
+                        Springbok Properties determines your home's estimated value based on recent home sales in your area.
+                    </p>
+                    <div class="row">
+                        <div class="col s12 m4">
+                            <input placeholder="Postcode" id="postcode" name="postcode" type="text" class="z-depth-1">
+                        </div>
+                        <div class="col s12 m4">
+                            <input placeholder="Email" id="email" type="email" class="z-depth-1">
+                        </div>
+                        <div class="col s12 m4">
+                            <input placeholder="Phone Number" id="phone" name="phone" type="text" class="z-depth-1">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m12 l4 offset-l4">
+                            <button type="submit" class="waves-effect waves-light btn-large btn-block sp-primary sp-secondary-text">Estimate Now</button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </section>
+
+        <section class="house-worth-estimate sp-secondary-text">
+            <div class="container">
+                <h1 class="big center-align">What IS SPRINGBOK's House Price Estimate?</h1>
+                <div class="row">
+                    <div class="col m4 s12">
+                        <div class="row">
+                            <div class="col s12 m11">
+                                <p class="center-align"><img src="assets/images/pin/graph.png" width="64" height="93"></p>
+                                <h4>How do you calculate it?</h4>
+                                <p class="big">
+                                    We use current & recent data of similar properties in your area that have sold or are currently on the market. Using price & sale comparables, helps you instantly estimate your property’s current market value — so you get an accurate online estimate, instantly.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col m4 s12">
+                        <div class="row">
+                            <div class="col s12 m11">
+                                <p class="center-align"><img src="assets/images/pin/calculator.png" width="64" height="93"></p>
+                                <h4>How do you calculate it?</h4>
+                                <p class="big">
+                                    Your house value estimate is powered by real-time property prices and details so you can find out what your house is worth easily. Check your house value to get the latest pricing based on property sale activity in your neighborhood.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col m4 s12">
+                        <div class="row">
+                            <div class="col s12 m11">
+                                <p class="center-align"><img src="assets/images/pin/bulls-eye.png" width="64" height="93"></p>
+                                <h4>How do you calculate it?</h4>
+                                <p class="big">
+                                    When you’re ready for professional house valuation, an experienced regional valuer can carry out a full property valuation at your house in person and give you price recommendations based on its unique characteristics and details. Talk to our experts that specialise in your area for a free consultation.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
---
+++ sprevamp/why-use-springbok-buy.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="why-buy-reasons sp-primary sp-secondary-text">
+            <div class="container center-align">
+                <h1 class="big">
+                    10 reasons to choose Springbok
+                </h1>
+                <p class="big">
+                    Since launching fast sale in 2012, Springbok have changed the UK housing market for good and have now become the number one property sales specialist in just few years.
+                </p>
+            </div>
+        </section>
+
+        <section class="why-buy-odd sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/1.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>1. Hundreds of properties</h3>
+                        <p class="big">
+                            We have hundreds of properties available. We list all sorts of homes for all sizes of mortgage,
+                            from efficient studio flats to massive five-bed family homes. Whatever your budget, Springbok
+                            has properties all over the UK for you to view. If you are an investor,
+                            <a href="#">click here</a> to get access to our exclusive investment deals
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="why-buy-even white sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/2.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>2. We're open 24/7</h3>
+                        <p class="big">
+                            Whether you want to arrange a viewing or just need more details on a property,
+                            we're here. Our lines are open 24/7 every single day of the year, so we never miss a call.
+                            And if you prefer email contact, we aim to respond to online enquiries within an
+                            hour - making sure you register your interest and move a step closer to ownership
+                            as quickly as possible. Go to our <a href="#">properties for sale</a> now.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="why-buy-odd sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/3.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>3. Expert help to find the right properties</h3>
+                        <p class="big">
+                            Tired of looking through all those hundreds of properties? Contact us and tell us your preferred areas. We'll take up the search and assist you in finding suitable properties in your desired location. Not only that, but we'll do some expert instant buyer-to-property matching to highlight your best options.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="why-buy-even white sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/4.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>4. Expert local searches</h3>
+                        <p class="big">
+                            Far too many online agents don't have a clue about the areas they sell in. Springbok does: our experienced sales team can give you hands-on assistance if you are unfamiliar with your target area. Looking for a property surrounded by the best transport links, schools or restaurants? Tell us what you want, and we'll pinpoint the ideal home!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="why-buy-odd sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/5.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>5. SMS and email alerts</h3>
+                        <p class="big">
+                            Stay connected by registering with Springbok. In addition to receiving property details from your dedicated negotiator, we'll send you instant email notifications whenever new properties become available that match your requirements. And once you're on the viewing train, we'll pop you SMS reminders for every appointment, to help you stay organised.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="why-buy-even white sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/6.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>6. Industry accreditation you can trust</h3>
+                        <p class="big">
+                            Springbok Properties is a proud member of The Property Ombudsman, the Trading Standards Institute and the Federation of Small Businesses. These memberships ensure that you will receive the highest level of customer service when you buy with us.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="why-buy-odd sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/7.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>7. Keeping in touch</h3>
+                        <p class="big">
+                            At Springbok, we don't believe there can ever be too much useful communication. Our expert sales team ensures you are kept updated whether you're house-hunting or completing a purchase. We'll track your viewings, offers made, offer acceptance and the status of the conveyancing process, and make sure you know exactly where you stand at any given moment.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="why-buy-even white sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/8.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>8. Property for dummies</h3>
+                        <p class="big">
+                            Property experts should never make you feel stupid, just because you don't know all the ins and outs of the process. At Springbok, we're happy to help with all your questions and worries, no matter how small or silly they may seem. We're here to take the hassle out of buying your new home, so lean on us and let us guide you.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="why-buy-odd sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/9.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>9. Springbok expert conveyancing</h3>
+                        <p class="big">
+                            We do everything we can to take the pain out of buying property. If you're not sure who to talk to, ask us about our recommended mortgage and financial advisers: they don't work for us, but we know they're reliable, trustworthy and honest. And even if you already have a family solicitor, talk to us about our recommended conveyancers - they're much faster than the average solicitor and can have you in your new home in no time!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="why-buy-even white sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m4 push-m8 center-align">
+                        <img src="assets/images/why-buy/10.png" width="335" height="264" class="responsive-img z-depth-1"/>
+                    </div>
+                    <div class="col s12 m8 pull-m4">
+                        <h3>
+                            10. Innovative Part Exchange Program
+                        </h3>
+                        <p class="big">
+                            Springbok is one of the only companies in the country to operate a part exchange program. With the help of our sister company, we organise a guaranteed sale for your home. That means you can make an offer on your dream house without all the hassle and worry of finding buyers for your current property!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
