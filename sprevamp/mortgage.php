5ae6dce636087730a6978eba4aa895e1ee818f74
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-07-24 16:27:31 +0600

    Added mortgage page

---
+++ sprevamp/mortgage.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="mortgage-banner no-padding sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m6 hide-on-small-only">
+                        &nbsp;
+                    </div>
+                    <div class="col s12 m6">
+                        <div class="shaded-form white">
+                            <div class="row">
+                                <div class="col s12 l10 offset-l1">
+                                    <h2 class="center-align">Don't have a mortgage yet?</h2>
+                                    <p class="center-align big">
+                                        Springbok's recommended financial advisors can help you. Just fill in the simple form below.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col s12 m10 offset-m1 l8 offset-l2">
+                                    <form>
+                                        <div class="classic-form medium">
+                                            <input placeholder="Name" id="name" name="name" type="text" class="z-depth-1">
+                                            <input placeholder="Email" id="email" type="email" class="z-depth-1">
+                                            <input placeholder="Postcode" id="postcode" name="postcode" type="text" class="z-depth-1">
+
+                                            <button type="submit" class="waves-effect waves-light btn-large btn-block sp-primary sp-secondary-text">Get Started</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="white sp-secondary-text center-align">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m10 offset-m1 l8 offset-l2">
+                        <h2>How Springbok's Recommended Financial Advisors Can Help</h2>
+                        <p class="big">
+                            Our recommended financial advisors are all independent experts who are FSA regulated. They do not charge fees for this free, no-obligation consultation. They are whole-market brokers with access to private and public banks and all mortgage lenders, so they can help you find a new mortgage or reduce your monthly payments and total mortgage debt. Connect with us and ask for more information.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="home-chat no-padding sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m8 l4">
+                        <h2>Want to Chat?</h2>
+                        <p class="big">Talk to our friendly experts who can help you make a better decision.</p>
+                    </div>
+                    <div class="col s12 m8 l5">
+                        <h2>We can call you back</h2>
+                        <form class="">
+                            <div class="classic-form no-padding">
+                                <div class="row margin-bottom-0">
+                                    <div class="col s6">
+                                        <input placeholder="Your Name" id="chat-name" name="chat-name" type="text">
+                                    </div>
+                                    <div class="col s6">
+                                        <input placeholder="Your Number" id="chat-phone" name="chat-phone" type="text">
+                                    </div>
+                                </div>
+                                <div class="right-align">
+                                    <button type="submit" class="waves-effect waves-light btn-large sp-secondary sp-primary-text">Call Me Back</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
