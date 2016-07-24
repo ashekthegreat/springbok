a4270050b31275d2ebd86b7ea1ba1570afbd0d74
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-07-24 21:25:17 +0600

    Added how-we-compare.php page

---
+++ sprevamp/how-we-compare.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="compare-main white sp-secondary-text">
+            <div class="container">
+                <h1 class="center-align">How we compare</h1>
+
+                <table class="table table-compare table-compare-online">
+                    <thead>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td><img src="assets/images/logo.png" width="152" height="45"></td>
+                            <td><img src="assets/images/vs.png" width="45" height="45" class="vs"/> Online Estate Agents</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Service</td>
+                            <td>Tailor-made, flexible, customised service that focuses on your needs. Includes everything from listing to buyer qualification, negotiation and specialist conveyancers.</td>
+                            <td>Often just a listing, leaving you to handle everything else.</td>
+                        </tr>
+                        <tr>
+                            <td>Success</td>
+                            <td>Paid after you complete and you have the money from your sale.</td>
+                            <td>Paid upfront and the million dollar question is why will they fight tooth and nail to get a better price for you when they already been paid in full.</td>
+                        </tr>
+                        <tr>
+                            <td>Fees</td>
+                            <td>No sale, no fees, and free valuation. No upfront estate agency fees.</td>
+                            <td>Listing fees and upfront agency fees to be paid even if the property never sells.</td>
+                        </tr>
+                        <tr>
+                            <td>For Sale boards</td>
+                            <td>Full contact details. <br><br>
+                                We pride ourselves on providing a premier service across the country, with mobile-friendly local phone numbers, so <br>
+                                buyers can call us immediately. <br><br>
+                                Enough staff to make sure someone picks up the phone and handles calls
+                                about your property.<br><br>
+                                Website designed to make finding more information and contacting us easier.
+                            </td>
+                            <td>Rarely more than a phone number on the board. Many boards have no contact information.<br><br>
+                                Website designed to reduce staff costs and push clients to use the big online portals, rather than make contact easier.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Extra Costs</td>
+                            <td>No extra costs or fees for selling your property.</td>
+                            <td>Most add on fees, increasing their headline-grabbing rates significantly.</td>
+                        </tr>
+                        <tr>
+                            <td>True cost</td>
+                            <td>We ensure you get the best price by giving you a qualified, experienced valuer, a qualified negotiator who contacts sellers and negotiates thousands extra.<br><br>
+                                We may cost a bit more, but we work hard to get you a lot more on your selling price!
+                            </td>
+                            <td>Cheaper for a reason: you end up paying more, just like when you buy a cheap car, a cheap holiday or a cheap meal.</td>
+                        </tr>
+                        <tr>
+                            <td>Additional Estate agent Fee</td>
+                            <td>One transparent fee, paid on success.</td>
+                            <td>Repeat fees if your property is not sold in a set period. They fail, you pay more</td>
+                        </tr>
+                        <tr>
+                            <td>Pricing</td>
+                            <td>Realistic market prices to sell the property fast</td>
+                            <td>Little or no help with pricing. First objective is the upfront fees, leaving you to guess and potentially under-sell.</td>
+                        </tr>
+                        <tr>
+                            <td>Practising what they preach</td>
+                            <td>Superstar negotiators who deliver excellent results. We will fight tooth and claw to get the best deal for you on the timescale you specify.</td>
+                            <td>Typically survive on investments, so your sale is more an annoyance than anything else. Rarely any real interest in helping you sell.</td>
+                        </tr>
+                        <tr>
+                            <td>Availability</td>
+                            <td>24 hours a day, every day.</td>
+                            <td>Limited telephone support, replaced by a lot of online form filling and chat boxes.</td>
+                        </tr>
+                        <tr>
+                            <td>Coverage</td>
+                            <td>Huge reach, creating remarkable success.</td>
+                            <td>Often small, unpopular, insignificant and offer limited coverage.</td>
+                        </tr>
+                        <tr>
+                            <td>Advertising</td>
+                            <td>Massive marketing that can be seen by over 30 million UK residents on an ever-growing number of sites.</td>
+                            <td>Many cannot use the best, premium online portals to promote your property</td>
+                        </tr>
+                        <tr>
+                            <td>Buyers</td>
+                            <td>All checked and qualified so there are no time-wasters and no chains.</td>
+                            <td>Everyone, regardless of whether they're really interested, just window shopping or are stuck in a chain. Easy online booking adds to the problem.</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <h3 class="center-align">How we compare to traditional estate agents</h3>
+                <table class="table table-compare table-compare-agents">
+                    <thead>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td><img src="assets/images/logo.png" width="152" height="45"></td>
+                            <td><img src="assets/images/vs.png" width="45" height="45" class="vs"/> Traditional Estate Agent</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Speed</td>
+                            <td>We always aim to sell in weeks.</td>
+                            <td>No timescale - usually months or even years.</td>
+                        </tr>
+                        <tr>
+                            <td>Fees</td>
+                            <td>No sale, no fees, and free valuation.</td>
+                            <td>Listing fees, withdrawal fees, viewing fees, marketing fees and any other fees they can think of.</td>
+                        </tr>
+                        <tr>
+                            <td>Pricing</td>
+                            <td>Realistic market prices to sell the property fast.</td>
+                            <td>Overvalued to convince you they can get more.</td>
+                        </tr>
+                        <tr>
+                            <td>Availability</td>
+                            <td>24 hours a day, every day.</td>
+                            <td>Office hours, maybe evenings if you're lucky.</td>
+                        </tr>
+                        <tr>
+                            <td>Coverage</td>
+                            <td>Unprecedented nationwide (even worldwide!) coverage with expert local focus.</td>
+                            <td>Purely local focus.</td>
+                        </tr>
+                        <tr>
+                            <td>Advertising</td>
+                            <td>Massive marketing that can be seen by over 30 million UK residents on an ever-growing number of sites.</td>
+                            <td>Local papers, pictures in their window and perhaps a few major online sites.</td>
+                        </tr>
+                        <tr>
+                            <td>Buyers</td>
+                            <td>All checked and qualified so there are no time-wasters and no chains.</td>
+                            <td>Everyone, regardless of whether they're really interested, just window shopping or are stuck in a chain.</td>
+                        </tr>
+                        <tr>
+                            <td>Services</td>
+                            <td>Tailor-made, flexible, customised and focused on your needs.</td>
+                            <td>One size fits all: inflexible, inefficient and built for people with months to waste.</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <h3 class="center-align">How We Compare To Cash Buyers</h3>
+                <table class="table table-compare table-compare-buyers">
+                    <thead>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td><img src="assets/images/logo.png" width="152" height="45"></td>
+                            <td><img src="assets/images/vs.png" width="45" height="45" class="vs"/> Cash Buyer</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Speed</td>
+                            <td>We always aim to sell in weeks.</td>
+                            <td>Aims to sell in weeks as well.</td>
+                        </tr>
+                        <tr>
+                            <td>Fees</td>
+                            <td>No sale, no fees, and free valuation.</td>
+                            <td>Depends very much on the company.</td>
+                        </tr>
+                        <tr>
+                            <td>Pricing</td>
+                            <td>Realistic market prices to sell the property fast.</td>
+                            <td>Discounted for a fast sale.</td>
+                        </tr>
+                        <tr>
+                            <td>Availability</td>
+                            <td>24 hours a day, every day.</td>
+                            <td>Usually just business hours and evenings (depending on the company).</td>
+                        </tr>
+                        <tr>
+                            <td>Coverage</td>
+                            <td>Unprecedented nationwide, (even worldwide!) coverage with expert local focus.</td>
+                            <td>Local focus with most companies.</td>
+                        </tr>
+                        <tr>
+                            <td>Services</td>
+                            <td>Tailor-made, flexible, customised and focused on your needs.</td>
+                            <td>Specifically focused on an immediate sale to solve financial problems.</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        </section>
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
