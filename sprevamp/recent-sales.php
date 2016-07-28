453053d23d21716e251fc7fdeceb2240cfcf3110
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-07-28 23:33:29 +0600

    added recent sales page and images

---
+++ sprevamp/recent-sales.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css" media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="reviews-banner no-padding sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row margin-bottom-0">
+                    <div class="col s12 m6 hide-on-small-only">
+                        &nbsp;
+                    </div>
+                    <div class="col s12 m6">
+                        <div class="big-play center-align">
+                            <h1>12 Success stories
+                                in under 3 minutes</h1>
+                            <a href="#" class="play" data-id="QedLPJ3dYXU">
+                                <img src="assets/images/play-yellow.png" width="88" height="88">
+                            </a>
+                            <p>Watch the video to find out more</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="sales-list white sp-secondary-text">
+            <div class="container center-align">
+                <h1 class="margin-bottom-40">Recent sales in London & South East England</h1>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Dagenham.png">
+                            </div>
+                            <div class="card-content center-align">Dagenham</div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Fareham.png">
+                            </div>
+                            <div class="card-content center-align">Fareham</div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Felpham.png">
+                            </div>
+                            <div class="card-content center-align">Felpham</div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Gosport.png">
+                            </div>
+                            <div class="card-content center-align">Gosport</div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Ilford.png">
+                            </div>
+                            <div class="card-content center-align">Ilford</div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Poplar.png">
+                            </div>
+                            <div class="card-content center-align">Poplar</div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Southend-on-sea.png">
+                            </div>
+                            <div class="card-content center-align">Southend-on-sea</div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Thamesmead.png">
+                            </div>
+                            <div class="card-content center-align">Thamesmead</div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Upminster.png">
+                            </div>
+                            <div class="card-content center-align">Upminster</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="white sp-secondary-text">
+            <div class="container center-align">
+                <h1 class="margin-bottom-40">Recent sales in London & South East England</h1>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Dagenham.png">
+                            </div>
+                            <div class="card-content center-align">Dagenham</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="white sp-secondary-text">
+            <div class="container center-align">
+                <h1 class="margin-bottom-40">Recent sales in London & South East England</h1>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Dagenham.png">
+                            </div>
+                            <div class="card-content center-align">Dagenham</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="white sp-secondary-text">
+            <div class="container center-align">
+                <h1 class="margin-bottom-40">Recent sales in London & South East England</h1>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Dagenham.png">
+                            </div>
+                            <div class="card-content center-align">Dagenham</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="white sp-secondary-text">
+            <div class="container center-align">
+                <h1 class="margin-bottom-40">Recent sales in London & South East England</h1>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Dagenham.png">
+                            </div>
+                            <div class="card-content center-align">Dagenham</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="white sp-secondary-text">
+            <div class="container center-align">
+                <h1 class="margin-bottom-40">Recent sales in London & South East England</h1>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Dagenham.png">
+                            </div>
+                            <div class="card-content center-align">Dagenham</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="white sp-secondary-text">
+            <div class="container center-align">
+                <h1 class="margin-bottom-40">Recent sales in London & South East England</h1>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img class="responsive-img materialboxed" src="assets/images/recent-sales/London%20&%20South%20East%20England/Dagenham.png">
+                            </div>
+                            <div class="card-content center-align">Dagenham</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
