1328ef6383f8e09af290e99d2e61a699407ca1d7
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-07-30 21:18:43 +0600

    added conveyance.php page

---
+++ sprevamp/conveyance.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="conveyance-banner sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m6 hide-on-small-only">
+                        &nbsp;
+                    </div>
+                    <div class="col s12 m6">
+                        <div class="shaded-form">
+                            <div class="row">
+                                <div class="col s12 l10 offset-l1">
+                                    <h3 class="center-align z-depth-1-text">Fast Track Your Conveyancing!</h3>
+                                    <p class="center-align big z-depth-1-text">
+                                        Springbok's recommended solicitors can help you. Just fill in the quick form below.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col s12 m10 offset-m1 l8 offset-l2">
+                                    <form>
+                                        <div class="classic-form medium">
+                                            <input placeholder="Your Name" id="name" name="name" type="text" class="z-depth-1">
+                                            <input placeholder="Your Email" id="email" type="email" class="z-depth-1">
+                                            <input placeholder="Your Number" id="phone" name="phone" type="text" class="z-depth-1">
+
+                                            <button type="submit" class="waves-effect waves-light btn-large btn-block sp-primary sp-secondary-text">Send an Enquiry</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="white sp-secondary-text center-align">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m10 offset-m1 l10 offset-l1">
+                        <h2>How Springbok's Solicitors Can Help?</h2>
+                        <p class="bigger">
+                            Our recommended panel of solicitors are experts in property conveyancing.
+                        </p>
+                        <p>
+                            Our main goal is to achieve a quick sale and complete the conveyancing  process within a timeframe to suit you and your buyer. Most importantly, we want to provide you with an excellent & seamless customer service experience. Solicitors are a significant part of this process. To avoid any hassle, we select from among hundreds the best of the best professionals to include on our recommended panel of solicitors.
+                        </p>
+                        <p class="bigger">
+                            Ask us to tell you more when you are connected.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
