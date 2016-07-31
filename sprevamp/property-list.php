32adabc398d92a07cc200b910a273ec529122813
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-08-01 00:11:10 +0600

    added basic of property list page

---
+++ sprevamp/property-list.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="properties-search sp-secondary darken-3 white-text no-padding">
+            <div class="container">
+                Search bar
+            </div>
+        </section>
+
+        <section class="properties-cards sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m12 l8">
+                        <div class="card card-horizontal sp-secondary-text clearfix">
+                            <div class="card-image">
+                                <img class="responsive-img" src="assets/images/success/halfpenny_lane.jpg" width="490" height="366">
+                            </div>
+                            <div class="card-content">
+                                <div class="center-align">
+                                    <h3 class="margin-top-0">Halfpenny Lane, Wakefield</h3>
+                                    <!--<a href="#" class="waves-effect waves-light btn sp-secondary sp-primary-text margin-top-15 margin-bottom-15">More Success Stories</a>-->
+                                    <div class="row margin-bottom-0">
+                                        <div class="col s12 m5">
+                                            <h4>1,070 Days</h4>
+                                            <p>Not sold with previews estate agents</p>
+                                        </div>
+                                        <div class="col s12 m4">
+                                            <h4>17 days</h4>
+                                            <p>Sale Agreed with Springbok</p>
+                                        </div>
+                                        <div class="col s12 m3">
+                                            <h4>100%</h4>
+                                            <p>Asking Price</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-action">
+                                <a href="#">This is a link</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col s12 m12 l4">
+                        <div class="row">
+                            <div class="col s4 m4 l12 margin-bottom-15">
+                                <a href="#" class="center-block">
+                                    <img src="assets/images/advertise/adv-1.jpg" width="300" height="260" class="responsive-img hoverable"/>
+                                </a>
+                            </div>
+                            <div class="col s4 m4 l12 margin-bottom-15">
+                                <a href="#">
+                                    <img src="assets/images/advertise/adv-2.jpg" width="300" height="260" class="responsive-img hoverable"/>
+                                </a>
+                            </div>
+                            <div class="col s4 m4 l12 margin-bottom-15">
+                                <a href="#">
+                                    <img src="assets/images/advertise/adv-3.jpg" width="300" height="260" class="responsive-img hoverable"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
