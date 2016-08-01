a4b78650f5793a71075edf6c5d77df94364b5452
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-08-02 01:34:14 +0600

    added springbok-charities page and contact-us page

---
+++ sprevamp/contact-us.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="contact-list white sp-secondary-text">
+            <div class="container">
+                <h2 class="margin-bottom-40 center-align">
+                    CONTACT US BY CALLING ON &nbsp;
+                    <small>
+                        <span class="nowrap">
+                            <i class="sprite sprite-call"></i>
+                            <a href="tel:08000684015">0800 068 4015</a> or
+                        </span>
+                        <a href="tel:03332007322" class="nowrap">0333 200 7322</a>
+                    </small>
+                </h2>
+
+                <div class="row">
+                    <div class="col s12 m6 l4">
+                        <div class="card sp-secondary lighten-5 sp-secondary-text hoverable">
+                            <div class="card-content center-align">
+                                <div class="card-title">Sell a House</div>
+                                <div class="card-body big">
+                                    <i class="sprite sprite-call"></i> Call (open 24/7)<br>
+                                    <a href="tel:08000687935" class="nowrap">0800 068 7935</a><br>
+                                    Or <a href="#">Book A Valuation</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <div class="card sp-secondary lighten-5 sp-secondary-text hoverable">
+                            <div class="card-content center-align">
+                                <div class="card-title">Book a viewing</div>
+                                <div class="card-body big">
+                                    <i class="sprite sprite-call"></i> Call (open 24/7)<br>
+                                    <a href="tel:08000684015" class="nowrap">0800 068 4015</a><br>
+                                    Or <a href="property-list.php">Search Properties For Sale</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <div class="card sp-secondary lighten-5 sp-secondary-text hoverable">
+                            <div class="card-content center-align">
+                                <div class="card-title">Investors</div>
+                                <div class="card-body big">
+                                    <i class="sprite sprite-call"></i> Call (open 24/7)<br>
+                                    <a href="tel:08000684015" class="nowrap">0800 068 4015</a><br>
+                                    Or <a href="#">See Investment Properties</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <div class="card sp-secondary lighten-5 sp-secondary-text hoverable">
+                            <div class="card-content center-align">
+                                <div class="card-title">Landlords</div>
+                                <div class="card-body big">
+                                    <i class="sprite sprite-call"></i> Call<br>
+                                    <a href="tel:08000484816" class="nowrap">0800 048 4816</a><br>
+                                    Or <a href="#">See Landlord Services</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <div class="card sp-secondary lighten-5 sp-secondary-text hoverable">
+                            <div class="card-content center-align">
+                                <div class="card-title">Need to rent?</div>
+                                <div class="card-body big">
+                                    <a href="#">See Properties To Rent</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <div class="card sp-secondary lighten-5 sp-secondary-text hoverable">
+                            <div class="card-content center-align">
+                                <div class="card-title">Sale progression</div>
+                                <div class="card-body big">
+                                    <i class="sprite sprite-call"></i> Call <a href="tel:08000687935" class="nowrap">0800 068 7935</a><br>
+                                    Or Email us on<br>
+                                    <a href="mailto:progression@springbokgroup.co.uk" class="small nowrap"><small>progression@springbokgroup.co.uk</small></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <div class="card sp-secondary lighten-5 sp-secondary-text hoverable">
+                            <div class="card-content center-align">
+                                <div class="card-title">Media / General</div>
+                                <div class="card-body big">
+                                    Email us on<br>
+                                    <a href="mailto:admin@springbokgroup.co.uk" class="small nowrap"><small>admin@springbokgroup.co.uk</small></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6 l4">
+                        <div class="card sp-secondary lighten-5 sp-secondary-text hoverable">
+                            <div class="card-content center-align">
+                                <div class="card-title">Complaints</div>
+                                <div class="card-body big">
+                                    Email us on<br>
+                                    <a href="mailto:customer.service@springbokgroup.co.uk" class="small nowrap"><small>customer.service@springbokgroup.co.uk</small></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
---
+++ sprevamp/springbok-charities.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="white sp-secondary-text">
+            <div class="container">
+                <h1 class="margin-bottom-40">Charities</h1>
+                <div class="row">
+                    <div class="col s12 m12 l5 margin-top-30 margin-bottom-30">
+                        <img src="assets/images/cancer-research-logo.png" height="154" width="400" class="responsive-img"/>
+                        <br>
+                        Our selected charity for 2015 – Cancer Research Fund UK
+                    </div>
+                    <div class="col s12 m12 l7">
+                        <h3>About Cancer Research fund UK</h3>
+
+                        <p>Cancer Research UK is the world's leading charity dedicated to beating cancer through research. Cancer Research UK pioneers life-saving cancer research to help beat cancer sooner. It is determined to fighting cancer on all fronts, finding new ways to prevent, diagnose and treat it to save more lives. With public funding initiatives we can ensure more people beat cancer.</p>
+
+                        <p>Cancer Research fund UK aims to save millions of lives by discovering new ways to prevent, diagnose and treat cancer, and survival has doubled over the past 40 years. Every day in the UK there are more than 400 people diagnosed with cancer that will survive the disease for more than 10 years thanks to research.</p>
+
+                        <p>If you wish to make a private donation please visit:<br>
+                        <a href="www.cancerresearchuk.org" target="_blank">www.cancerresearchuk.org</a><br>
+                        <a href="mailto:customer.services@cancer.org.uk">customer.services@cancer.org.uk</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
