d64098b99ced84851fa14a6b84d01b6eda5c1c79
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-08-03 00:49:06 +0600

    added springbok-team page

---
+++ sprevamp/springbok-team.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="team-banner no-padding sp-secondary lighten-5 sp-secondary-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m6">
+                        <img src="assets/images/team.jpg" width="946" height="530" class="responsive-img hide-on-med-and-up">
+                        <span class="hide-on-small-only">&nbsp;</span>
+                    </div>
+                    <div class="col s12 m6 center-align shaded">
+                        <div class="margin-bottom-40 margin-top-40">
+                            <h1 class="margin-bottom-0">We are springbook</h1>
+                            <p class="biggest margin-bottom-40 margin-top-0">Nice to meet you!</p>
+                            <p class="big">Here's how one of our clients describes us:</p>
+                            <p>
+                                "Actually the most amazing experience we have ever had, these guys are not
+                                'estate agents' in the true sense of the word - they are totally on your side,
+                                totally human, and fantastic. Every part of the process, from the valuation to
+                                completion, was exceptional. I can't sing their praises enough."
+                                <strong><em>- Elizabeth & Sally</em></strong>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="white sp-secondary-text center-align">
+            <div class="container">
+                <p class="big">Springbok Properties offers a market-beating alternative to the traditional way of selling property.</p>
+                <p class="big">Our entire organisation revolves around our dedication to a single mission - to meet our customer's primary need.</p>
+                <p class="big">We provide the best customer service humanly possible, and sell your property on your timescale, with the best possible price.</p>
+                <p class="big">At Springbok, customer service isn't a department: it's a job requirement for every single employee.</p>
+            </div>
+        </section>
+
+        <section class="sp-secondary lighten-5 sp-secondary-text center-align">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m12 l10 offset-l1">
+                        <h1 class="margin-bottom-0">Our customers love us</h1>
+                        <p class="biggest margin-bottom-40 margin-top-0">But you don't have to take our word for it...</p>
+                        <p class="big">
+                            "<strong>Does what it says on the tin</strong> - excellent communications throughout - always willing to answer the most simple of questions without patronising my ignorance."
+                            <strong><em>- Dave Sutheran, TS24</em></strong>
+                        </p>
+                        <p class="big">
+                            "Springbok is a <strong>very professional and customer-oriented organisation</strong>. Thank you very much guys - I am really happy with the service I had from you."
+                            <strong><em>- Tosef Akhtar, M18</em></strong>
+                        </p>
+                        <p class="big">
+                            "I have been so impressed with everyone in your business. I cannot speak highly enough of the professional attitude. I have especially valued the "<strong>personal touch</strong>" and the way that I have been kept updated on the proceedings, during what can be such a stressful time."
+                            <strong><em>- Debbie Humphreys</em></strong>
+                        </p>
+                        <p class="big">
+                            "Brilliant service, well done Springbok."
+                            <strong><em>- Nick Dunne, LA9</em></strong>
+                        </p>
+                        <p class="big">
+                            "<strong>Prompt, friendly and always calm and reassuring</strong> - they sorted my sale out and organised all the legal aspects for me. It was one of the easiest sales I've ever had to complete-thanks!"
+                            <strong><em>- David W., KY4</em></strong>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
