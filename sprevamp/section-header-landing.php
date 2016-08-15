68af5c90a6181fff1c2520db467c8fad1a745e02
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-08-16 01:24:01 +0600

    landing page header revert

---
+++ sprevamp/section-header-landing.php
@@ -0,0 +1,8 @@
+<div class="navbar-fixed landing">
+    <nav>
+        <div class="nav-wrapper container">
+            <a href="index.php" class="brand-logo left"><img src="assets/images/logo.png" width="152" height="45"></a>
+        </div>
+
+    </nav>
+</div>
