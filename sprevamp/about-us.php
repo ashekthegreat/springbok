aeebd16fecf17ef843bad745ffc34a56c367aa68
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-08-29 21:19:22 +0600

    Updated about-us

--- sprevamp/about-us.php
+++ sprevamp/about-us.php
@@ -12,7 +12,7 @@
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css" media="screen,projection"/>
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -23,38 +23,140 @@
 
     <main>
 
-        <section class="team-banner no-padding sp-secondary lighten-5 sp-secondary-text">
+        <section class="white sp-secondary-text center-align">
             <div class="container">
-                <div class="row">
-                    <div class="col s12 m6">
-                        <img src="assets/images/team.jpg" width="946" height="530" class="responsive-img hide-on-med-and-up">
-                        <span class="hide-on-small-only">&nbsp;</span>
-                    </div>
-                    <div class="col s12 m6 center-align shaded">
-                        <div class="margin-bottom-40 margin-top-40">
-                            <h1 class="margin-bottom-0">We are Springbok</h1>
-                            <p class="biggest margin-bottom-40 margin-top-0">Nice to meet you!</p>
-                            <p>
-                                "Actually the most amazing experience we have ever had, these guys are not
-                                'estate agents' in the true sense of the word - they are totally on your side,
-                                totally human, and fantastic. Every part of the process, from the valuation to
-                                completion, was exceptional. I can't sing their praises enough."
-                                <strong><em>- Elizabeth & Sally</em></strong>
-                            </p>
-                        </div>
-                    </div>
-                </div>
+                <h1>About Springbok</h1>
+                <p class="big">It is not what we say that is important, but what others say about us. We turn clients into fans and grow our business by exceeding all expectations.</p>
             </div>
         </section>
 
-        <section class="white sp-secondary-text center-align">
+        <section class="sp-secondary lighten-5 sp-secondary-text">
             <div class="container">
-                <p class="big">Springbok Properties offers a market-beating alternative to the traditional way of selling property.</p>
-                <p class="big">Our entire organisation is dedicated to a single mission - meeting our customer's needs.</p>
-                <p class="big">We provide the best customer service humanly possible, and sell your property on your timescale, with the best possible price.</p>
-                <p class="big">At Springbok, customer service isn't a department: it's a job requirement for every single employee.</p>
+                <h3>Our Happy Customers</h3>
+                <blockquote>
+                    <p>"Actually the most amazing experience we have ever had, <b>these guys are not 'estate agents' in the true sense of the word</b> - they are totally on your side, totally human, and fantastic. Every part of the process from the valuation to completion was exceptional. <b>I can't sing their praises enough</b>. The house was on the <b>market for a matter of days before we had offers</b> on the table, and in this market that's just awesome! We were <b>on the market with another estate agent for months without even a bite, and now look at us, sold, done, dusted</b> with no drama and on our way to the sunshine!! I would highly recommend Springbok, every member of staff a delight, the process easy and the results AMAZEBALLS!!! Thank you to everyone at Springbok"</p>
+                    <footer>Elizabeth and Sall</footer>
+                </blockquote>
+                <h3>Our 24/7 Service</h3>
+                <blockquote>
+                    <p>"Really good service. Matthew got back to me within an hour of calling to enquire about the property and managed to get me a viewing <b>within 2 hours, on the day I called (Sunday).</b> Lauren kept me up to date with every step of the sale and sometimes even more than my solicitor! Excellent and friendly service and I would definitely recommend this company."</p>
+                    <footer>KY, M44</footer>
+                </blockquote>
+                <h3>Our Values</h3>
+                <blockquote>
+                    <p>"I was very happy with the service I received from Springbok. They were pro-active with their aim of pushing through the sale as quickly as possible. I was contacted every few days to discuss where I was personally with the sale, and provided with updates regarding solicitors and the seller. Kate was always extremely friendly and polite and easy to deal with."</p>
+                    <footer>John Mellor, HD8</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"I recently bought a property through Springbok Properties. My dealings with all the Springbok Properties staff especially Natalie were all good. They were <b>very attentive and proactive, making sure I was aware of every step which</b> was being taken to complete the transaction. <b>I would certainly recommend their company to anyone going off this</b> experience and hope to do business with them again sometime. Many thanks."</p>
+                    <footer>Paul, OL4</footer>
+                </blockquote>
+                <h3>Our Transparent Service</h3>
+                <blockquote>
+                    <p>"I have to say that I did not know who Springbok were, <b>but once you have dealt with them you wish that all agents were as clear and transparent.</b> Natalie worked through all the challenges with me and supported me through the buying process. She understood what was required and kept me up to date reactively and proactively. <b>I would certainly use the organisation again.</b>"</p>
+                    <footer>Sandip Patel</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"<b>I found that I was kept in the loop</b> throughout the process of buying a property. If I rang up and Natalie was not available, she rang me back. Help and advice was always available. I would use them again in the future. <b>I have had bad service in the past from a high street agent and this agent has renewed my confidence.</b>"</p>
+                    <footer>Liz Handerson</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"I would recommend this company to anyone wishing to sell their property quickly without any fuss. My property had been for sale for 2 years with conventional estate agents, and I was disillusioned and very disappointed with their lack of results. Springbok were <b>professional from the start, providing regular updates and advice</b>. All the stress of dealing with the actual sale was handled by them, too. <b>I would use them again without a second thought</b>. Thank you so much!</p>
+                    <footer>Adele Clayton</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"Springbok sold my property after it was on the market with other agents for over 12 months. Both Lee and Natalie, who dealt with me, were helpful, courteous and <b>kept me informed of what was happening every step of the way</b>. They made what can be a worrying and stressful process a lot, lot easier. I would recommend anyone thinking of selling their property to use them."</p>
+                    <footer>Gragg Mogg, B45</footer>
+                </blockquote>
+                <h3>Our Empathy</h3>
+                <blockquote>
+                    <p>"I had to sell a family property as executor, following a close family bereavement. The <b>process of selling is bad enough, let alone doing it following a family death. James and Jenna were on hand every time I phoned - this was reassuring as I lived the other end of the country! Prompt, friendly and always calm and reassuring</b> - they sorted my sale out and organised all the legal aspects for me. It was one of the easiest sales I've ever had to complete - thanks!"</p>
+                    <footer>David W., KY4</footer>
+                </blockquote>
+                <h3>Our Speed &amp; More Money</h3>
+                <blockquote>
+                    <p>"Springbok kept me well informed at all times and the sale just whizzed through<b>, I couldn't believe how quick and efficient they were</b>!"</p>
+                </blockquote>
+                <blockquote>
+                    <p>"Never heard of Springbok before but seemed to offer what I was looking for (a quick sale). Following discussions on options, I decided to give Springbok a chance for a set period. I was not disappointed. Totally professional, kept me well informed, dealt with problems without making a fuss, provided customer care and service not often found today. <b>Also they sold my house quickly for a bit more than I had agreed on. </b>Many thanks."</p>
+                    <footer>Tom Savage</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"I recently sold my house using Springbok. The service I received was excellent, everything was explained to me very carefully prior to signing a contract with them, the For Sale sign went up on a Saturday, we had an unsuccessful viewing on the Monday, and again on the Tuesday, but on Wednesday a couple viewed who immediately loved the house. They came back again the following Saturday and put an offer in which we rejected. They came back 2 days later with another offer which we accepted, all done within 2 weeks, everything went through very smoothly. <b>Both parties were very happy with the speedy outcome, would not hesitate to recommend Springbok to family and friends</b>. All members of staff I had dealings with were very helpful, particularly Jenna."</p>
+                    <footer>Valerie Wirral, CH44</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"From start to finish, we found Springbok Properties most helpful, reliable. The staff were polite and eager to sell your property <b>for the best possible price and in the shortest time possible</b>. We found their service very efficient and ideal for a quick sale of our property. Charlotte, who took care of our sale, <b>was excellent, she kept us informed constantly. </b>I would recommend Springbok highly for a quick and efficient sale of property."</p>
+                    <footer>Mr &amp; Mrs D Cotton</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"I found all the staff I dealt with at Springbok to be friendly, helpful and professional at all times. Promises of return phone calls were always kept and nothing was too much trouble. The house went on the market on the Friday, I had 14 <b>viewings over the next 3 days and it was sold for more than the asking price on the Tuesda</b>y. Everything went smoothly, with no hitches at all. It has been a pleasure to deal with Springbok and <b>I would not hesitate to recommend their services to anyone wanting to sell a house</b>"</p>
+                    <footer>Ian Wilson, LS29</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"An excellent service. I was most impressed with the whole process from initial contact through to completion. Communication was always prompt and efficient, staff always friendly, polite and willing to help. Most importantly, in an area where houses are <b>notoriously slow to sell, Springbok found me a buyer within two weeks </b>and completion was made in less than 2 months - proving that their approach really does work."</p>
+                    <footer>Peter Wartbuton</footer>
+                </blockquote>
+                <h3>Our Communication</h3>
+                <blockquote>
+                    <p>"At every stage we were kept <b>in the picture about what was happening</b>. Return calls were prompt and information relevant and timely. Excellent advice regarding how best to market our property, e.g. at what point it may be worthwhile considering re-pricing or alternative measures. Good liaising skills between vendor and purchaser."</p>
+                </blockquote>
+                <blockquote>
+                    <p>"I found my house purchase with <a href="https://www.youtube.com/watch?v=BYKz90GY5EM" target="_blank" style="color:#333; text-decoration:none; font-weight:normal">Springbok Properties</a> to complete quickly and efficiently with no delays or problems. Staff were friendly and kept in <b>contact frequently with any updates.</b>"</p>
+                    <footer>Wendy Gary</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"<b>Good estate agents, not local but that's not been a problem as only a phone call away.</b> If unable to take the call, will always ring you back promptly. Staff very helpful and nothing too much trouble. The house was sold quite quickly, the second couple to view put an offer in and we accepted. Easy as that and before you knew it, we were moving. <b>If you want to sell your property fast look no further</b>."</p>
+                    <footer>Suzanne Walton</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"I had very clear and good communication along the way. They even took time to speak to me about myself and were not in a rush to end conversations. This <b>put a personal edge on the communications and made the process a lot more of a happy one</b>. Thanks."</p>
+                    <footer>Michael, WF2</footer>
+                </blockquote>
+                <h3>Our Customer Service</h3>
+                <blockquote>
+                    <p>"From start to finish, the company were very professional, keeping us informed of what was happening every step of the way, making it as stress-free as possible. <b>They were very caring and gave excellent customer service.</b> Even giving us a courtesy call after we had completed. We would definitely recommend them to our friends and family. I would score them 10 out of 10."</p>
+                    <footer>Susan North</footer>
+                </blockquote>
+                <h3>Our Proactive Perseverance</h3>
+                <blockquote>
+                    <p>"I want to place on record my thanks to both James and Natalie for their extremely professional help and support in the marketing and selling of my property. <b>Once the collapse of our first offer was confirmed, James had prospective buyers through the doors in no time, which resulted in a successful deal.</b> Natalie was fantastic, keeping me up to date all the way through to the sale's completion. I certainly will recommend Springbok Properties to any potential seller. Many thanks once again."</p>
+                    <footer>Keith</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"I found Springbok staff very helpful and informative. I was kept up to date with the progress of the property sale. The property sold within 2 weeks of it being advertised. Although this sale fell through, staff were eager to get the property back on the market and it sold again quickly for a second time."</p>
+                    <footer>Amanda Heaton</footer>
+                </blockquote>
+                <h3>Our Excellence</h3>
+                <blockquote>
+                    <p>"The overall help from the staff at Springbok was very good. <b>Their customer skills are excellent</b>. They were very friendly and very helpful in the process of selling my property. Out of all of the staff, I would personally say that Natalie Jackson was the most helpful, as she always kept me informed over what was going on. She was always very kind and very positive. I would recommend Springbok to anyone I know who is going to either sell or buy a property in the future."</p>
+                    <footer>Leigh Stewart, L36</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"After attempting to purchase 2 properties<b> through other estate agents and spending the whole time feeling I was either being constantly misled or ignored (or both at once), </b>I stumbled on a property where the agent just happened to be Springbok Properties. What luck!</p>
+                    <p><b>I must say that I don't ever go out of my way to write a review for anything less than exceptional service, which is exactly what I received from start to finish whilst purchasing a property via Springbok</b>. At no point did I feel like anybody at Springbok was doing anything other than being <b>100% honest (which is hard to find in an estate agent)</b> and whatever my query was, I would ring them and they would always do everything to help me out as quickly as possible. Whenever I spoke to anybody, I felt like they were there to help me with whatever I wanted (as opposed to other estate agents where every time I spoke to them it felt like they were only there to fill their pockets or say anything to get me off the phone in the hope somebody else picked up next time).</p>
+                    <p>Overall, I would give Springbok 9/10 (Although I would give Natalie, who took ownership of the property I was purchasing part way through, a 10/10). When me and my partner both get promoted and feel it's time to upgrade to a new house, I honestly cannot see myself looking to anywhere other than Springbok to deal with the purchase/sale.<b> Thank you Springbok for making what can be a very stressful period in life as easy as possible</b>."</p>
+                    <footer>Shalshaska, M20</footer>
+                </blockquote>
+                <blockquote>
+                    <p><b>"Fantastic customer service. Very good communication.</b> Supportive before offer entered. Honest info about the property. Fully supported after offer was accepted and kept well informed of what was going on. Fantastic solicitors that were recommended by Springbok. Overall great to deal with."</p>
+                    <footer>Lauren, OL16</footer>
+                </blockquote>
+                <blockquote>
+                    <p>"I contacted Springbok Properties as I saw the advert in the local paper and decided to use them, as having previous experience with other estate agents told me that they are one of the best. Natalie was kind, courteous, helpful and kept me informed about everything that was required. If anyone that I know was selling their house, would recommend them."</p>
+                    <footer>Gillian Gillard</footer>
+                </blockquote>
             </div>
         </section>
+        <section class="white sp-secondary-text">
+            <div class="container">
+                <h3></h3>
+                <blockquote>
+
+                </blockquote>
+            </div>
+        </section>
+
 
         <section class="sp-secondary lighten-5 sp-secondary-text center-align">
             <div class="container">
