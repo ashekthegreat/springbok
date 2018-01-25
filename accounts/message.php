eb233a0473372fa5f71aac5b1e3fa3b10e1b9197
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2018-01-25 17:28:16 +0600

    Updated chat

---
+++ accounts/message.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize<?php echo in_array( $_SERVER['REMOTE_ADDR'], array( '127.0.0.1', '::1' ))?'':'.min' ?>.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+    <?php include("section-header.php"); ?>
+
+    <main>
+
+        <section class="sp-secondary-text section-message">
+            <div class="container narrow">
+
+                <ul class="collapsible" data-collapsible="accordion">
+                    <li>
+                        <div class="collapsible-header sp-secondary white-text active">7, Dudley Walk , TS10 2LN, Redcar</div>
+                        <div class="collapsible-body">
+                            <div class="chat-items">
+                                <div class="chat-item you">
+                                    <div class="name">Adam Sandler</div>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Blanditiis consequatur ipsa nihil non officia pariatur quia quis reprehenderit sequi sint!
+                                </div>
+                                <div class="chat-item you">
+                                    <div class="name">Adam Sandler</div>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Blanditiis consequatur ipsa nihil non officia pariatur quia quis reprehenderit sequi sint!
+                                </div>
+                                <div class="chat-item me">
+                                    <div class="name">Simone Arnison</div>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Blanditiis consequatur ipsa nihil non officia pariatur quia quis reprehenderit sequi sint!
+                                </div>
+                            </div>
+                            <div class="chat-write">
+
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+
+                <ul class="collapsible" data-collapsible="accordion">
+                    <li>
+                        <div class="collapsible-header sp-secondary white-text">7, Dudley Walk , TS10 2LN, Redcar</div>
+                        <div class="collapsible-body">
+                            <div class="chat-items">
+                                <div class="chat-item you">
+                                    <div class="name">Adam Sandler</div>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Blanditiis consequatur ipsa nihil non officia pariatur quia quis reprehenderit sequi sint!
+                                </div>
+                                <div class="chat-item you">
+                                    <div class="name">Adam Sandler</div>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Blanditiis consequatur ipsa nihil non officia pariatur quia quis reprehenderit sequi sint!
+                                </div>
+                                <div class="chat-item me">
+                                    <div class="name">Simone Arnison</div>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Blanditiis consequatur ipsa nihil non officia pariatur quia quis reprehenderit sequi sint!
+                                </div>
+                                <div class="chat-item you">
+                                    <div class="name">Adam Sandler</div>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                    Blanditiis consequatur ipsa nihil non officia pariatur quia quis reprehenderit sequi sint!
+                                </div>
+                            </div>
+                            <div class="chat-write">
+
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+        </section>
+
+    </main>
+
+    <?php include("section-footer.php"); ?>
+</body>
+</html>
