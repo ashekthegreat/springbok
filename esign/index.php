90b1e6dd37b40e61a9df8b71bb294827d9469e51
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2018-02-07 17:33:27 +0600

    added esign

---
+++ esign/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+
+    <title>Springbok Properties - Get Up To 100% Value For Your House Quickly</title>
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="assets/css/materialize.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <script type="text/javascript" src="assets/js/jquery-2.2.4.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-migrate-1.4.1.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.ui.touch-punch.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.signature.min.js"></script>
+
+    <script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+
+<body>
+    <header>
+        <div class="container padding-top-15 padding-bottom-20 sp-secondary-text">
+            <a href="index.php" class="brand-logo left"><img src="assets/images/logo.png" width="152" height="45"></a>
+        </div>
+    </header>
+
+    <main>
+
+        <section class="section-esign-form white sp-secondary-text">
+            <div class="container">
+                <form target="#">
+
+                    <h1 class="center-align margin-bottom-30">Sole Selling Rights Agreement</h1>
+
+                    <h4>Section 1 - Sole Selling Rights Instruction Form</h4>
+                    <p>
+                        This document contains the Terms and Conditions of Business agreed between you the
+                        client (the "<strong>Seller</strong>" or "<strong>You</strong>" or "<strong>Your</strong>") and
+                        Springbok Properties. By signing this agreement You accept the terms set out in this document will be binding to You.
+                    </p>
+
+                    <div class="fields">
+                        <div class="input-group prefix">
+                            <span class="input-group-addon">Seller(s)</span>
+                            <input type="text" name="seller">
+                        </div>
+
+                        <div class="input-group prefix">
+                            <span class="input-group-addon">Property to be sold:</span>
+                            <input type="text" name="property">
+                        </div>
+
+                        <div class="input-group prefix">
+                            <span class="input-group-addon">Marketing Price: Offers In Excess of &pound;</span>
+                            <input type="text" name="marketing-price">
+                        </div>
+
+                        <div class="input-group prefix">
+                            <span class="input-group-addon">Sole Selling Rights Period 20 weeks from the Start Date</span>
+                            <input type="text" name="period">
+                        </div>
+
+                        <p>Start date – The date of the signature to this agreement or if later, one day after the notice period to terminate the engagement of the previous estate agent of the Seller(s) ends.</p>
+
+                        <div class="input-group prefix">
+                            <span class="input-group-addon">Selling Fees with standard solicitors package &pound;</span>
+                            <input type="text" name="fees">
+                        </div>
+
+                    </div>
+
+                    <h4 class="center-align text-underline margin-top-30">
+                        Terms & Conditions:  Please Read This Legal Document Thoroughly before Signing
+                    </h4>
+
+                    <div class="terms small">
+                        <p>1. Sole Selling Rights: You give Springbok Properties Ltd  "Springbok Properties" the sole rights to market the Property for sale on Your behalf, directly or using any third party sub-contractor(s) during the Sole Selling Rights Period (but this does not include any financial, tax, or legal advice or representation).</p>
+                        <p>2. Duration & Termination: This agreement starts on the Start Date. It will continue initially for the Sole Selling Rights Period and, after that, indefinitely until terminated by either party giving the other party a written termination notice of at least 28 days. Any such notice will be deemed to have been served 2 days after being posted first class or sent by email. 4. Marketing Price: The Marketing Price does not represent a valuation of the Property and may be changed upon instruction from you. Such a change in price does not affect this agreement.</p>
+                        <p>3. Property Insurance: You will maintain adequate building, contents and occupiers/public liability insurance to cover people attending the Property for inspections, viewings or other purposes with Your consent (and agree that Springbok Properties is not liable for any loss or damage arising from such attendances).</p>
+                        <p>4. Standard Solicitors Package Option: The Standard Solicitors Package Option covers legal fee, bank transfer, Title Register and a Plan for the sale of your Property. Springbok Properties will only pay selling solicitors’ costs if you use one of the Springbok Properties’ recommended panel of solicitors. If you decide to use a different solicitor, you will still be liable to pay the full selling fee/commission.</p>
+                        <p>5. Remuneration:</p>
+                        <p>5.1 The "remuneration" due to Springbok Properties under this agreement the fees set out in Section 3 of the Instruction Form. Accordingly, subject to clauses 5.2 and 5.3, pursuant to the requirements of The Estate Agents (Provision of Information) Regulations, 1991, You will be liable to pay remuneration to Springbok Properties, in each of the following circumstances:</p>
+                        <p>5.1.1 if unconditional contracts for the sale of the Property are exchanged within the Sole Selling Rights Period, even if the purchaser was not found by Springbok Properties but by another agent or by any other person, including Yourself; or</p>
+                        <p>5.1.2 if unconditional contracts for the sale of the Property are exchanged within 9 months after the expiry of the Sole Selling Rights Period but to a purchaser who was introduced to You by Springbok Properties during that Period or with whom Springbok Properties had negotiations about the Property during that Period; or</p>
+                        <p>5.1.3 if the option to market the property before the end of the Cooling Off Period is selected (Clause 6 On Seller(s) Confirmations/Acknowledgements), and you exercise your right to cancel this agreement during the Cooling Off Period, and unconditional contracts of sale of the property are exchanged within 9 months from the date of introduction, to a purchaser who was introduced to You by Springbok Properties prior to you serving a notice of cancellation.</p>
+                        <p>5.2 If the Property is located in Scotland, each reference to the words "unconditional contracts for the sale of the Property are exchanged" in clause 14 will be deemed replaced by "unconditional missives for the sale of the Property are concluded".</p>
+                        <p>5.3 For the purposes of clause 14.1, Springbok Properties need not be the effective cause of the Property being sold.</p>
+                        <p>5.4 The said remuneration is payable in full (and without any deduction by way of offset or otherwise) within 7 days of the date of completion. In the event of any account that is not settled in full within 7 days from the date of completion, Springbok Properties reserve the right to charge interest on the balance of 4% above HSBC Bank PLC base rate at the time of completion. Springbok Properties also reserve the right to charge You any legal fees and costs that they may incur due to non-payment of any remuneration due.</p>
+                        <p>5.5 This agreement operates as an irrevocable instruction to the solicitor(s) or conveyancer(s) acting for You to pay our remuneration from any sale proceeds (and is also an irrevocable authority to such solicitor(s) or conveyancer(s) acting for You in the transaction to provide Springbok Properties with any details in relation to the sale transaction and any personal information they may have, such as forwarding address and contact telephone numbers).</p>
+                        <p>5.6 We require payment to terms: Payment must be made on completion, in full and without any deduction, set off or counterclaim. In the event that an account is outstanding for more than 7 days, we will refer the matter to our debt collection agents as appointed by ourselves, which will incur external third party debt collection agency recovery costs plus VAT at the prevailing rate, for which you will be liable and which will be added to the debt sought from you.</p>
+                        <p>5.7 In the event that Springbok Properties ascertains that You have breached clause 5.1 by not disclosing to Springbok Properties that unconditional contracts for sale of the Property have been exchanged, in order to avoid payment of remuneration, You will be liable to pay Springbok Properties the fee as specified in the Selling Fees of Section 1 of the Instruction Form being payable within 7 days upon request by Springbok properties.</p>
+                        <p>6. Cooling Off Period: You are permitted, under the Consumer Protection (Information, Cancellation and Additional Charges) Regulations 2013, to cancel your agreement with Springbok Properties if you do so in writing, within 14 days of signing the Sole Selling Rights Agreement. Notice of cancellation must be in writing and sent to Cancellation Department, Springbok Properties, 1 Universal Square, Devonshire Street North, Manchester, M12 6JH. Any cancellation takes effect from the date of recorded posting by mail.</p>
+                        <p>7. General Points: The laws of England and Wales apply to this agreement (and English courts have exclusive jurisdiction in respect of it). A purported variation to this agreement is only valid and binding if made expressly in writing by Springbok Properties and at least one of the Sellers. Any purported waiver of any right or remedy of a party is only valid and binding if made in writing. The parties will keep each other informed of their respective contact details. If the Seller comprises two or more people in any capacity or role, then all such people will comply with (and be liable under) this agreement on a joint and several basis. Headings are for reference only and do not affect the interpretation of the terms of this agreement.</p>
+                    </div>
+
+                    <div class="signature-container margin-top-25">
+                        <div class="row margin-bottom-15">
+                            <div class="col s12 m3">Seller Signature</div>
+                            <div class="col s12 m9">
+                                <div id="signature-area">
+                                    <div id="signature-canvas"></div>
+                                    <div id="clear-canvas">X</div>
+                                </div>
+                                <textarea name="signature" id="signature" class="hide"></textarea>
+                            </div>
+                        </div>
+                        <div class="row margin-bottom-15">
+                            <div class="col s12 m3">Name</div>
+                            <div class="col s12 m9">
+                                <input id="name" name="name" type="text" class="" value="">
+                            </div>
+                        </div>
+                        <div class="right-align">
+                            <button type="submit" name="btn-sign" id="btn-sign" class="waves-effect waves-light btn btn-block-on-small sp-primary sp-secondary-text">Click Here To Sign</button>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </section>
+
+    </main>
+
+    <footer class="margin-top-50">
+        <div class="container sp-secondary-text smaller center-align">
+            <div>
+                <span class="nowrap">Springbok Properties Ltd</span>
+                Head Office: 1 Universal Square, Devonshire Street North, Manchester.
+                <span class="nowrap">M12 6JH</span>
+            </div>
+            <div class="margin-top-15 margin-bottom-40">
+                <span class="nowrap">Company Registration No. 09045757</span>
+                <span class="nowrap margin-left-40 margin-right-40">VAT Registration No. 189899309</span>
+                <span class="nowrap">Data Protection Registration No. ZA065473</span>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ esign/section-header.php
@@ -0,0 +1,84 @@
+<div class="navbar-fixed">
+    <nav>
+        <div class="nav-wrapper container">
+            <a href="index.php" class="brand-logo left"><img src="assets/images/logo.png" width="152" height="45"></a>
+            <a href="#" data-activates="main-sidenav" class="button-collapse menu-button right"><strong>MENU</strong></a>
+            <!--<a href="tel:03332007322" class="button-collapse call-button right"><b class="sprite sprite-call"></b></a>-->
+            <ul id="main-nav" class="right hide-on-med-and-down">
+                <li><a href="javascript:;" class="dropdown-main-nav" data-activates="dropdown-sell">Sell</a></li>
+                <li><a href="free-valuation.php">Free Valuation</a></li>
+                <li><a href="property-list.php">Search Properties</a></li>
+                <li><a href="investments.php">Investments</a></li>
+                <li><a href="reviews.php">Reviews</a></li>
+                <li><a href="#" class="dropdown-main-nav" data-activates="dropdown-advice">Property Advice</a></li>
+            </ul>
+            <ul class="side-nav sp-secondary lighten-5" id="main-sidenav">
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                        <li>
+                            <a class="collapsible-header">Sell</a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <li><a href="#home-success">Success Stories</a></li>
+                                    <li><a href="#home-how-it-works">How It Works</a></li>
+                                    <li><a href="#home-estimator">Free Offer Estimator</a></li>
+                                    <li><a href="#home-video">Video Reviews</a></li>
+                                    <li><a href="#home-recent-sale">Our Recent Sales</a></li>
+                                    <li><a href="#home-customer-service">Customer Service</a></li>
+                                    <li><a href="#home-success-numbers">Success In Numbers</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li><a href="free-valuation.php">Free Valuation</a></li>
+                <li><a href="property-list.php">Search Properties</a></li>
+                <li><a href="investments.php">Investments</a></li>
+                <li><a href="reviews.php">Reviews</a></li>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                        <li>
+                            <a class="collapsible-header">Property Advice</a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <li><a href="why-use-springbok-buy.php">Why Buy With Springbok</a></li>
+                                    <li><a href="buyers-guide.php">Buyers Guide</a></li>
+                                    <li><a href="#">Latest News & Updates</a></li>
+                                    <li><a href="mortgage.php">Mortgages</a></li>
+                                    <li><a href="conveyance.php">Conveyance</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <div class="sub-nav">
+            <div class="container right-align">
+                <span class="call-button sp-secondary-text">
+                    <span class="nowrap call-statement">Call Your Croydon Expert Now</span>
+                    <span class="margin-left-20">24/7</span>
+                    <i class="sprite sprite-call-247 margin-right-5"></i>
+                    <span class="click-to-call">0142 770 0300</span>
+                </span>
+            </div>
+        </div>
+    </nav>
+
+    <ul id="dropdown-sell" class="dropdown-content">
+        <li><a href="#home-success">Success Stories</a></li>
+        <li><a href="#home-how-it-works">How It Works</a></li>
+        <li><a href="#home-estimator">Free Offer Estimator</a></li>
+        <li><a href="#home-video">Video Reviews</a></li>
+        <li><a href="#home-recent-sale">Our Recent Sales</a></li>
+        <li><a href="#home-customer-service">Customer Service</a></li>
+        <li><a href="#home-success-numbers">Success In Numbers</a></li>
+    </ul>
+    <ul id="dropdown-advice" class="dropdown-content">
+        <li><a href="why-use-springbok-buy.php">Why Buy With Springbok</a></li>
+        <li><a href="buyers-guide.php">Buyers Guide</a></li>
+        <li><a href="#">Latest News & Updates</a></li>
+        <li><a href="mortgage.php">Mortgages</a></li>
+        <li><a href="conveyance.php">Conveyance</a></li>
+    </ul>
+</div>
